893925f3e9f7756eb636a9fffc278dda14560cdf
prasannaa09 <prasanna.alur.09@gmail.com>
2025-03-03 01:08:30 +0530

    backend part completed. table created

--- src/main/java/com/example/bmrcl/repository/MetroStationRepository.java
+++ src/main/java/com/example/bmrcl/repository/MetroStationRepository.java
@@ -5,5 +5,9 @@ import com.example.bmrcl.entity.MetroStation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
-public interface MetroStationRepository extends JpaRepository<MetroStation, Long> {}
+public interface MetroStationRepository extends JpaRepository<MetroStation, Long> {
+    Optional<MetroStation> findByName(String name);  // Add this method
+}
